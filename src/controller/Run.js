7db7401dd6591e831778503b6f474d1522d1f3a8
Orlando <opseua@gmail.com>
2023-03-11 18:23:09 -0300

    .

--- src/controller/Run.js
+++ src/controller/Run.js
@@ -1,6 +1,4 @@
 const axios = require('axios');
-const sendResponse = require('../../server');
-
 
 const Run = {
 
@@ -17,20 +15,22 @@ const Run = {
 
     Script1: async (inf1, inf2) => {
         try {
+
             const result = await eval(inf1);
-            console.log("TERMINANDO");
-            sendResponse(res.end("teste"), res_tudo);
             return result;
+
         }
         catch (error) {
-
+            
             // RETORNAR ERRO 'esperar-nao'. DEFINIR O LINK DE RESPOSTA
             if (inf2 == "esperar-nao") {
                 var fim_resposta_err = `DEU ERRO: ${error.message}`;
                 var fim_link = inf1.match(/\/\*NAO_APAGAR_1\*\/(.*?)\/\*NAO_APAGAR_2\*\//)[1].replaceAll("fim_resposta", "fim_resposta_err");
                 eval(fim_link);
             }
+
             return `DEU ERRO: ${error.message}`;
+
         };
     }
 }
