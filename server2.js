9a428b51e91f5e362a0014ea78957f13d996d4bd
Orlando <opseua@gmail.com>
2023-03-11 20:18:23 -0300

    .

--- server2.js
+++ server2.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const bodyParser = require('body-parser');
+const { spawn } = require('child_process');
 
 const axios = require('axios');
 const Run = require('./src/controller/Run.js');
@@ -7,42 +8,64 @@ const bl = `
 `;
 
 const server = http.createServer((req, res) => {
+    bodyParser.text()(req, res, async () => {
 
-  // Configurando o body-parser para processar o corpo da solicitação como texto
-  bodyParser.text()(req, res, () => {
+        const url1 = req.url.slice(-1) == '/' ? req.url : `${req.url}/`;
+        const url2 = url1.split('/');
+        const rota = url2.length > 2 ? `${url2[1]}` : 'VAZIO-ROTA';
+        const id = url2.length > 3 ? `${url2[2]}` : 'VAZIO-ID';
+        const body = Object.keys(req.body).length > 0 ? `${req.body}` : 'VAZIO-BODY';
 
-    const url1 = (req.url.slice(-1) == "/") ? req.url : `${req.url}/`
-    const url2 = url1.split("/");
-    const rota = (url2.length > 2) ? `${url2[1]}` : `VAZIO-ROTA`
-    const id = (url2.length > 3) ? `${url2[2]}` : `VAZIO-ID`
-    const body = (Object.keys(req.body).length > 0) ? `${req.body}` : `VAZIO-BODY`
+        /*         console.log('INICIO 1');
+                await new Promise((resolve) => setTimeout(resolve, 2500));
+                console.log('INICIO 2');
+                await new Promise((resolve) => setTimeout(resolve, 2500));
+                console.log('INICIO 3'); */
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Obrigado por sua solicitação!');
+        if (rota == "esperar-sim") {
+            var resultado = await Run.Script1(body, rota);
+            const res_api = `<script>Script1</script>${bl}<status>OK</status>${bl}<rota>${rota}</rota>${bl}<id>${id}</id>`;
+            const res_run = `<resposta>${bl}${resultado}${bl}</resposta>`;
+            const res_body = id == '1' ? `<body>${bl}${body}${bl}</body>` : `<body>VAZIO-[0]</body>`;
+            const res_tudo = `${res_api}${bl}${bl}${res_run}${bl}${bl}${res_body}`;
+            res.end(`${res_tudo}`);
+            console.log(`${res_tudo}`);
+            console.log(`${bl}ENCERROU esperar-sim`);
+            return
+        };
 
-    setTimeout(() => {
-      const res_api = `<script>Script1</script>${bl}<status>AGUARDANDO</status>${bl}<rota>${rota}</rota>${bl}<id>${id}</id>`;
-      const res_run = `<resposta>AGUARDANDO</resposta>`;
-      const res_body = `<body>VAZIO-[0]</body>`;
-      Run.Script1(body, "esperar-nao")
-        .then((resultado) => {
-          console.log(`RESULTADO FINAL: ${resultado}`)
-        })
+        if (rota == "esperar-nao") {
+            const res_api = `<script>Script1</script>${bl}<status>AGUARDANDO</status>${bl}<rota>${rota}</rota>${bl}<id>${id}</id>`;
+            const res_run = `<resposta>AGUARDANDO</resposta>`;
+            const res_body = id == '1' ? `<body>${bl}${body}${bl}</body>` : `<body>VAZIO-[0]</body>`;
+            const res_tudo = `${res_api}${bl}${bl}${res_run}${bl}${bl}${res_body}`;
+
+            /*console.log("RUN 1");
+            var resultado = Run.Script1(body, rota);
+            console.log("RUN 2");*/
 
 
+/*             const { spawn } = require('child_process');
+            const childProcess = spawn('node', ['code2.js', body, rota]);
+            childProcess.stdout.on; */
+            Run.Script1(body, rota);
+            await new Promise(resolve => setTimeout(resolve, 10000));
 
-      console.log('Continuando a execução do código...');
-    }, 5000);
+            res.end(`${res_tudo}`);
+            console.log(`${res_tudo}`);
+            console.log(`${bl}ENCERROU esperar-nao`);
+            return
+        };
 
-
-
-
-    /*     // Enviando o corpo da solicitação como resposta do servidor
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`Body da solicitação: ${body}`); */
-  });
+        res.end('ROTA/ID NÃO DEFINIDOS!');
+        return
+
+    });
 });
 
+
+
 server.listen(3000, () => {
-  console.log('Servidor escutando na porta 3000');
+    console.log('Servidor escutando na porta 3000');
 });
