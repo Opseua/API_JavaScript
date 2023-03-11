173f9e5df68a085ed706b12c1b80356b1f26af4c
Orlando <opseua@gmail.com>
2023-03-11 05:11:04 -0300

    .

--- src/controller/Rota1.js
+++ src/controller/Rota1.js
@@ -50,7 +50,7 @@ const Rota1 = {
         const res_api = `<script>Script1</script>${bl}<status>AGUARDANDO</status>${bl}<rota>${rota}</rota>${bl}<id>${id}</id>`;
         const res_run = `<resposta>AGUARDANDO</resposta>`;
         const res_body = `<body>VAZIO-[0]</body>`;
-        res.write('txt').send(`${res_api}${bl}${bl}${res_run}${bl}${bl}${res_body}`);
+        res.type('txt').end(`${res_api}${bl}${bl}${res_run}${bl}${bl}${res_body}`);
         Run.Script1(body, "esperar-nao")
           .then((resultado) => {
             console.log(`RESULTADO FINAL: ${resultado}`)
