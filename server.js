1c5dc643e8ce7baa3e2055b4928c3afb5299f733
Orlando <opseua@gmail.com>
2023-03-11 23:17:34 -0300

    .

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const http = require('http');
 const bodyParser = require('body-parser');
-const { spawn } = require('child_process');
-
-/* const controller = new AbortController();
-const signal = controller.signal; */
-
 const axios = require('axios');
 const Run = require('./src/controller/Run.js');
 const bl = `
@@ -19,12 +13,6 @@ const server = http.createServer((req, res) => {
         const rota = url2.length > 2 ? `${url2[1]}` : 'VAZIO-ROTA';
         const id = url2.length > 3 ? `${url2[2]}` : 'VAZIO-ID';
         const body = Object.keys(req.body).length > 0 ? `${req.body}` : 'VAZIO-BODY';
-
-        /*         console.log('INICIO 1');
-                await new Promise((resolve) => setTimeout(resolve, 2500));
-                console.log('INICIO 2');
-                await new Promise((resolve) => setTimeout(resolve, 2500));
-                console.log('INICIO 3'); */
 
         if (rota == "esperar-sim") {
             var resultado = await Run.Script1(body, rota);
@@ -45,33 +33,15 @@ const server = http.createServer((req, res) => {
             const res_tudo = `${res_api}${bl}${bl}${res_run}${bl}${bl}${res_body}`;
             res.end(`${res_tudo}`);
             console.log(`${res_tudo}${bl}${bl}ENCERROU esperar-nao`);
-        };
-
-        if (rota == "instancia-1") {
-            const res_api = `<script>Script1</script>${bl}<status>AGUARDANDO-OUTRA-INSTANCIA</status>${bl}<rota>${rota}</rota>${bl}<id>${id}</id>`;
-            const res_run = `<resposta>AGUARDANDO-OUTRA-INSTANCIA</resposta>`;
-            const res_body = id == '1' ? `<body>${bl}${body}${bl}</body>` : `<body>VAZIO-[0]</body>`;
-            const res_tudo = `${res_api}${bl}${bl}${res_run}${bl}${bl}${res_body}`;
-
-            /*             axios.post(`http://localhost:3001/${rota}/${id}`, body, { headers: { 'Content-Type': 'text/plain' } })
-                            .then(response => { console.log("ENVIADO PARA O OUTRO SERVIDOR"); }).catch(error => { console.error(error); });
-             */
-
-            console.log("segunda instancia");
-            Run.Script1(body, rota);
-
-
-
             return
         };
 
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('ROTA/ID NÃO DEFINIDOS!');
+        res.end('ROTA/ID NAO ENCONTRADOS!');
         return
 
     });
 });
-
 
 
 server.listen(3000, () => {
